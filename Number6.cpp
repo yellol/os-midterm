17fe00aff456d45f34464d57da3a05be29e30833
yellol <46012526+yellol@users.noreply.github.com>
2025-03-24 02:10:39 -0500

    implement input loop + fix parallel

--- Number6.cpp
+++ Number6.cpp
@@ -12,69 +12,73 @@ using namespace std;
 
 int main()
 {
-    string programName;
-    int instanceAmount = 1;
-    char mode = 'p';
-
-    cout << "closh> ";
-    cin >> programName;
-
-    cout << "count> ";
-    cin >> instanceAmount;
-
-    cout << "[p]arallel or [s]equential> ";
-    cin >> mode;
-
-    programName = "./" + programName;
-
-    if (mode == 'p')
+    while (true)
     {
-        cout << endl << "Initializing parallel processes..." << endl;
-        for (int i = 0; i < instanceAmount; i++) 
-        {
-            pid_t pid = fork();
+        string programName;
+        int instanceAmount = 1;
+        char mode = 'p';
 
-            if (pid == 0) 
+        cout << "closh> ";
+        cin >> programName;
+
+        cout << "count> ";
+        cin >> instanceAmount;
+
+        cout << "[p]arallel or [s]equential or e[x]it> ";
+        cin >> mode;
+
+        programName = "./" + programName;
+
+        if (mode == 'p')
+        {
+            cout << endl << "Initializing parallel processes..." << endl;
+            for (int i = 0; i < instanceAmount; i++) 
             {
-                char* args[] = { (char*)programName.c_str(), NULL };
-                execvp(programName.c_str(), args);
-                perror("execlp failed");
-                exit(1);
+                pid_t pid = fork();
+
+                if (pid == 0) 
+                {
+                    char* args[] = { (char*)programName.c_str(), NULL };
+                    execvp(programName.c_str(), args);
+                    perror("execlp failed");
+                    exit(1);
+                }
+                else if (pid < 0)
+                {
+                    perror("fork failed");
+                }
             }
-            else
+        } 
+        else if (mode == 's')
+        {
+            cout << endl << "Initializing sequential processes..." << endl;
+            for (int i = 0; i < instanceAmount; i++) 
             {
-                perror("fork failed");
-                exit(1);
+                pid_t pid = fork();
+
+                if (pid == 0) 
+                {
+                    char* args[] = { (char*)programName.c_str(), NULL };
+                    execvp(programName.c_str(), args);
+                    perror("execlp failed");
+                    exit(1);
+                }
+                else if (pid > 0) 
+                { 
+                    waitpid(pid, NULL, 0);
+                } 
+                else if (pid < 0) 
+                {
+                    perror("fork failed");
+                    exit(1);
+                }
             }
         }
-    } 
-    else if (mode == 's')
-    {
-        cout << endl << "Initializing sequential processes..." << endl;
-        for (int i = 0; i < instanceAmount; i++) 
+        else
         {
-            pid_t pid = fork();
-
-            if (pid == 0) 
-            {
-                char* args[] = { (char*)programName.c_str(), NULL };
-                execvp(programName.c_str(), args);
-                perror("execlp failed");
-                exit(1);
-            }
-            else if (pid > 0) 
-            { 
-                waitpid(pid, NULL, 0);
-            } 
-            else
-            {
-                perror("fork failed");
-                exit(1);
-            }
+            break;
         }
     }
-
-    
     return 0;
 }
 
